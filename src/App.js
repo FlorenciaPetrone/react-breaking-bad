2062b8884d969fd38b917f94eb4a210fc88c81cb
Florencia Petrone <florenciapetrone@Florencias-MacBook-Pro.local>
2021-04-30 16:30:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//React
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+//Components
+import Home from "./pages/Home";
+import Favorite from "./pages/Favorite";
+import Header from "./components/ui/Header";
+import Footer from "./components/ui/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/favorite" component={Favorite} />
+      </Switch>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ui/Footer.js
@@ -0,0 +1,17 @@
+//React
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="social-container">
+      <a href="https://github.com/FlorenciaPetrone">
+        <i class="fab fa-github-square fa-2x" />
+      </a>
+      <a href="https://www.linkedin.com/in/florencia-petrone/">
+        <i class="fab fa-linkedin fa-2x" />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
